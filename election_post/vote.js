46b4cbb73aa92536adf5d8715e6032df5463194a
Patrick Dawkins <pjcdawkins@gmail.com>
2012-06-07 22:20:43 +0100

    Added some JavaScript to grey out rankings when they've already been used in the ballot form.

--- election_post/vote.js
+++ election_post/vote.js
@@ -1,1 +1,21 @@
+/**
+ * @file
+ * JavaScript for the ballot paper form in the Election Post module.
+ */
+(function ($) {
 
+  $(document).ready(function() {
+
+    var inputs = $('#election-post-vote-form').find(':input[name^="candidate"]');
+
+    inputs.bind('click change', function() {
+      var elem = $(this), others = inputs.not(elem), val = elem.val();
+      others.find('option[disabled]').removeAttr('disabled');
+      if (val.length && val != 'NONE') {
+        others.find('option[value="' + val + '"]').not(':selected').attr('disabled', 'disabled');
+      }
+    });
+
+  });
+
+})(jQuery);
