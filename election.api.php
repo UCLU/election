0c9d6f2ec9accfc68c6a90b845f013b1a5f5a78e
Patrick Dawkins <p.dawkins@ucl.ac.uk>
2012-08-22 17:11:26 +0200

    Fixed 'save votes' callback and added help to election.api.php.

--- election.api.php
+++ election.api.php
@@ -28,8 +28,18 @@
  *   - has candidates: Whether the election type has candidates. Modules
  *     providing elections that have candidates should depend on the
  *     election_candidate submodule.
- *   - results page: A page callback that shows results for the election (used
- *     by the election_results submodule).
+ *   - save votes: The name of a function that saves 'votes' for this election
+ *     type to the {election_vote} table. This function runs inside the
+ *     same database transaction that saves 'ballots' to the {election_ballot}
+ *     table. To roll back the transaction it would throw an exception. The
+ *     function takes the parameters:
+ *         array $form        - The form array from election_vote_form.
+ *         array $form_state  - The form_state array from election_vote_form.
+ *         stdClass $post     - The election post object.
+ *         stdClass $election - The election object.
+ *         int $ballot_id     - The ID of the ballot that is being saved.
+ *   - results page: A page callback that shows results for elections of this
+ *     type (used by the election_results submodule).
  *   - results download form: A Form API form callback (taking the parameters
  *     $form, &$form_state, $post) that allows users to download the election's
  *     results.
