6b2f78aab47a800083e6961347dddf93d0b83cc3
Patrick Dawkins <p.dawkins@ucl.ac.uk>
2013-01-24 18:35:40 +0000

    Issue #1897760: added contextual links for elections, posts and candidates.

--- election_candidate/election-candidate.tpl.php
+++ election_candidate/election-candidate.tpl.php
@@ -9,7 +9,9 @@
 <div id="election-<?php print $election->election_id; ?>-candidate-<?php print $candidate->candidate_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php if (!$page): ?>
+    <?php if (isset($title_prefix)) { print render($title_prefix); } ?>
     <h2<?php print $title_attributes; ?>><a href="<?php print $candidate_url; ?>"><?php print $full_name; ?></a></h2>
+    <?php if (isset($title_suffix)) { print render($title_suffix); } ?>
   <?php endif; ?>
 
   <div class="content"<?php print $content_attributes; ?>>
--- election_post/election-post.tpl.php
+++ election_post/election-post.tpl.php
@@ -7,7 +7,9 @@
 <div id="election-<?php print $election->election_id; ?>-post-<?php print $post->post_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php if (!$page): ?>
+    <?php if (isset($title_prefix)) { print render($title_prefix); } ?>
     <h2<?php print $title_attributes; ?>><a href="<?php print $post_url; ?>"><?php print $title; ?></a></h2>
+    <?php if (isset($title_suffix)) { print render($title_suffix); } ?>
   <?php endif; ?>
 
   <div class="content"<?php print $content_attributes; ?>>
