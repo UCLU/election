013bcb28b96d9ee3bf8bbd43f75d9216dfbec850
Patrick Dawkins <p.dawkins@ucl.ac.uk>
2013-01-24 18:24:51 +0000

    Fixed display of titles for elections and posts.

--- election_post/election-post.tpl.php
+++ election_post/election-post.tpl.php
@@ -7,7 +7,7 @@
 <div id="election-<?php print $election->election_id; ?>-post-<?php print $post->post_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php if (!$page): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $post_url; ?>"><?php print render($content['title']); ?></a></h2>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $post_url; ?>"><?php print $title; ?></a></h2>
   <?php endif; ?>
 
   <div class="content"<?php print $content_attributes; ?>>
