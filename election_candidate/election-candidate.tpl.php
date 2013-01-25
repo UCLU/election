d93b38154f9b0c6bd9361a316b2060125444ff03
Patrick Dawkins <p.dawkins@ucl.ac.uk>
2013-01-25 15:06:26 +0000

    Clean-up

--- election_candidate/election-candidate.tpl.php
+++ election_candidate/election-candidate.tpl.php
@@ -9,9 +9,13 @@
 <div id="election-<?php print $election->election_id; ?>-candidate-<?php print $candidate->candidate_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php if (!$page): ?>
-    <?php if (isset($title_prefix)) { print render($title_prefix); } ?>
+    <?php if (isset($title_prefix)): ?>
+      <?php print render($title_prefix); ?>
+    <?php endif; ?>
     <h2<?php print $title_attributes; ?>><a href="<?php print $candidate_url; ?>"><?php print $full_name; ?></a></h2>
-    <?php if (isset($title_suffix)) { print render($title_suffix); } ?>
+    <?php if (isset($title_suffix)): ?>
+      <?php print render($title_suffix); ?>
+    <?php endif; ?>
   <?php endif; ?>
 
   <div class="content"<?php print $content_attributes; ?>>
--- election_post/election-post.tpl.php
+++ election_post/election-post.tpl.php
@@ -7,9 +7,13 @@
 <div id="election-<?php print $election->election_id; ?>-post-<?php print $post->post_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php if (!$page): ?>
-    <?php if (isset($title_prefix)) { print render($title_prefix); } ?>
+    <?php if (isset($title_prefix)): ?>
+      <?php print render($title_prefix); ?>
+    <?php endif; ?>
     <h2<?php print $title_attributes; ?>><a href="<?php print $post_url; ?>"><?php print $title; ?></a></h2>
-    <?php if (isset($title_suffix)) { print render($title_suffix); } ?>
+    <?php if (isset($title_suffix)): ?>
+      <?php print render($title_suffix); ?>
+    <?php endif; ?>
   <?php endif; ?>
 
   <div class="content"<?php print $content_attributes; ?>>
