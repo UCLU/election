cf35d15052b15569853f5e20c8f17b945213131b
Patrick Dawkins <pjcdawkins@gmail.com>
2014-06-03 12:11:14 +0100

    election_condition: make conditions more flexible.
    API change: the required 'callbacks' key for each condition has been changed to a 'callback', and the 'data' key has been added.
    
    See election_condition.api.php.

--- election_condition/election_condition.api.php
+++ election_condition/election_condition.api.php
@@ -12,20 +12,23 @@
  * @return array
  *   A structured election condition array, keyed by a unique machine name. Each
  *   election condition array can contain:
- *   - name: (Required) The name of the condition.
- *   - callbacks: (Required) An array of function names for the conditions. Each
- *     function takes two arguments - the election post object, and the user
- *     account object - and it must return TRUE if the user passes the
+ *   - name: (Required) The human-readable, translated name of the condition.
+ *   - callback: A callback for testing the condition. If not specified, the
+ *     condition machine name will be treated as a callback function. The
+ *     callback takes at least two arguments: the election post object and the
+ *     user account object. The callback must return TRUE if the user passes the
  *     condition.
+ *   - data: (Optional) An optional extra argument to pass to the callback.
  *   - description: A description of the condition for administrators.
  *   - user explanation: An explanation of the condition for users.
  */
 function hook_election_condition_info() {
   return array(
-    'gmail' => array(
+    'example_gmail' => array(
       'name' => t('Have a Gmail account'),
-      'callbacks' => array('check_gmail'),
-      'description' => t('The user\'s registered email address must end with "gmail.com" or "googlemail.com".'),
+      'callback' => 'condition_to_check_email_domain',
+      'data' => array('gmail.com'),
+      'description' => t('The user\'s registered email address must end with "gmail.com".'),
       'user explanation' => t('You must have a GMail account'),
     ),
   );
@@ -35,6 +38,6 @@ function hook_election_condition_info() {
  * Alter previously defined voting conditions.
  */
 function hook_election_condition_info_alter(&$info) {
-  // Add an extra check to the gmail condition.
-  $info['gmail']['callbacks'][] = 'check_gmail_extra';
+  // Add an extra email domain to the example_gmail condition.
+  $info['example_gmail']['data'][] = 'googlemail.com';
 }
